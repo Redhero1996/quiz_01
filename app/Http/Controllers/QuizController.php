0a1dd7dad395a7d25a8d905af7e37c02a70a13dd
Hero Gustin <frog.dbsk.cass@gmail.com>
2018-10-04 22:14:51 +0700

    create test page

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -9,14 +9,36 @@ class QuizController extends Controller
 {
     public function category($category_id)
     {
-        $topics = Topic::where('category_id', $category_id)->latest('id')->get(
-            [
-                'id',
-                'name',
-                'created_at',
-            ],
-        );
+        $topics = Topic::where('category_id', $category_id)->latest('id')->get();
+        $category = Category::whereId($category_id)->get();
 
-        return response()->json($topics);
+        return response()->json([
+            'topics' => $topics,
+            'category_slug' => $category[0]['slug'],
+        ]);
+    }
+
+    public function quiz($category_slug, $topic_slug)
+    {
+        $alphabet = [
+            'A', 'B', 'C', 'D', 'E',
+            'F', 'G', 'H', 'I', 'K',
+            'K', 'L', 'M', 'N', 'O',
+            'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y',
+            'Z',
+        ];
+        $topic = Topic::whereSlug($topic_slug)->first();
+        $questions = $topic->questions()->where('topic_id', $topic->id)->get();
+        $data = [];
+        foreach ($questions as $key => $question) {
+            $answers = Answer::where('question_id', $question->id)->get();
+            $data[$key] = [
+                'question' => $question,
+                'answers' => $answers,
+            ];
+        }
+
+        return view('pages.quiz', compact('topic', 'data', 'alphabet'));
     }
 }
---
+++ config/constants.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'number_ques' => '1',
+    'minute' => '15',
+    'second' => '00',
+];
